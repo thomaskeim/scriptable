404300bdec1c7b15cfb215b8404e3490173ab23f
thomaskeim <76742790+thomaskeim@users.noreply.github.com>
2020-12-29 17:18:36 +0100

    Update COVID-19 Widget.js

--- COVID-19 Widget.js
+++ COVID-19 Widget.js
@@ -1,9 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: first-aid;
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: red; icon-glyph: first-aid;
 // Licence: Robert Koch-Institut (RKI), dl-de/by-2-0
 
 
